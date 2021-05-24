fbb09d7e70931bf0d5ccb467bdf7354a3bbd475e
m-gamao <mmgamao1@gmail.com>
2021-05-23 23:58:40 -0400

    edited fav container

--- src/Pages/Favorites/containers/FavoritesContainer.js
+++ src/Pages/Favorites/containers/FavoritesContainer.js
@@ -6,12 +6,13 @@ import FavoritesForm from "../components/FavoritesForm";
 import "../../../assets/SearchContainer.css";
 import FavoritesResults from "../components/FavoritesResults";
 import "../../../assets/Main.css";
+import "../../../assets/FavoritesContainer.css";
 
 class FavoritesContainer extends Component {
   // BELOW CODE IS THE FAVORITES SEARCH FORM FOR USER TO ENTER QUERY ********************
   render() {
     return (
-      <div id="main">
+      <div className="main">
         <h1>Saved Recipes</h1>
         <FavoritesForm />
         <FavoritesResults />
